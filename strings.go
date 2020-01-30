1a15d43d9546f7975f2e037b03f33d994237d9cc
Nikolai <soven@wisebits.com>
2020-01-30 14:32:54 +0200

    add same site attribute to cookies (fix)

--- strings.go
+++ strings.go
@@ -56,6 +56,7 @@ var (
 	strContentRange     = []byte(HeaderContentRange)
 	strAuthorization    = []byte(HeaderAuthorization)
 
+<<<<<<< HEAD
 	strCookieExpires        = []byte("expires")
 	strCookieDomain         = []byte("domain")
 	strCookiePath           = []byte("path")
@@ -66,6 +67,17 @@ var (
 	strCookieSameSiteLax    = []byte("Lax")
 	strCookieSameSiteStrict = []byte("Strict")
 	strCookieSameSiteNone   = []byte("None")
+=======
+	strCookieExpires  		= []byte("expires")
+	strCookieDomain   		= []byte("domain")
+	strCookiePath     		= []byte("path")
+	strCookieHTTPOnly 		= []byte("HttpOnly")
+	strCookieSecure   		= []byte("secure")
+	strCookieMaxAge   		= []byte("max-age")
+	strCookieSameSite       = []byte("SameSite")
+	strCookieSameSiteLax    = []byte("Lax")
+	strCookieSameSiteStrict = []byte("Strict")
+>>>>>>> add same site attribute to cookies (fix)
 
 	strClose               = []byte("close")
 	strGzip                = []byte("gzip")
